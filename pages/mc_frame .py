be33f890fc0c0f686fbbe5681ea666518a2d9d55
design-ss <135007494+design-ss@users.noreply.github.com>
2023-08-01 10:09:31 +0900

    Update mc_frame .py

--- pages/mc_frame .py
+++ pages/mc_frame .py
@@ -114,34 +114,6 @@ with export_button1:
                 # ギルドフレームを保存
                 resized_224 = guild_image.resize((112, 272))
                 binary_dict["/frame/guild_frame/112x272/" + GUILD.name] = resized_224
-                
-                ####################################
-
-                #　640 × 640、320 ×　320　のリサイズ
-
-                ####################################
-                
-                # 画像ファイルを開く
-                icon_image = Image.open(ICON)
-
-                # 300 × 300 にリサイズしておく
-                image = icon_image.resize((300, 300))
-
-                # 透明画像を作っておく
-                blank_image_640 = Image.new('RGBA', (640, 640), (0, 0, 0, 0))
-
-                # 元の画像を貼り付け
-                left = (blank_image_640.width - image.width) // 2
-                top = int((blank_image_640.height - image.height) * 0.8)
-                blank_image_640.paste(image, (left, top))
-                resized_640 = blank_image_640
-
-                # 320リサイズ
-                resized_320 = resized_640.resize((320, 320))  # resizing the 640x640 image to 320x320
-
-                # 統合した画像の保存
-                binary_dict["/frame/guild_frame/320x320/" + GUILD.name] = resized_320
-                binary_dict["/frame/guild_frame/640x640/" + GUILD.name] = resized_640
 
                     
             time.sleep(3)
